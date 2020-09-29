d3baa0124c5037cb33c16d11d6de1b78988bd71d
Alex Gurin <gurin.fear@gmail.com>
2020-09-30 01:48:38 +0400

    Initial Commit

---
+++ ToyotaApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToyotaApp
+//
+//  Created by Алексей Гурин on 30.09.2020.
+//  Copyright © 2020 Алексей Гурин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
