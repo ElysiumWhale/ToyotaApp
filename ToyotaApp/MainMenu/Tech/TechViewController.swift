762644c4c66b2953e4aff677e9e479af678e1d73
Alex Gurin <gurin.fear@gmail.com>
2020-09-30 19:56:06 +0400

    Main Manu navigation

---
+++ ToyotaApp/MainMenu/Tech/TechViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TechViewController.swift
+//  ToyotaApp
+//
+//  Created by Алексей Гурин on 30.09.2020.
+//  Copyright © 2020 Алексей Гурин. All rights reserved.
+//
+
+import UIKit
+
+class TechViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
