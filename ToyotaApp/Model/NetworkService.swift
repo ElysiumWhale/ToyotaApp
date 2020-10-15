18b0e80de247164e3d2ad80a740a8428055af028
Alex Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2020-10-16 01:02:35 +0400

    Network request update

--- ToyotaApp/Model/NetworkService.swift
+++ ToyotaApp/Model/NetworkService.swift
@@ -1,11 +1,25 @@
 import Foundation
 
 class NetworkService {
+    
     public static let shared: NetworkService = NetworkService()
     
     let session = URLSession(configuration: URLSessionConfiguration.default)
-    private let mainUrl = URL(string: "http://cv39623.tmweb.ru/avtosalon/mobile/")
-    private let profileId = URLQueryItem(name: "id", value: "")
+    private var mainUrl: URLComponents
+    
+    init() {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "http" //MAKE ME HTTPS
+        urlComponents.host = "cv39623.tmweb.ru"
+        urlComponents.path = "/avtosalon/mobile/"
+        mainUrl = urlComponents
+    }
+    
+    private var profileId: URLQueryItem {
+        get {
+             URLQueryItem(name: "id", value: UserDefaults.standard.string(forKey: "user_id"))
+        }
+    }
     
     func makeRequest(with url: URL, completion: @escaping (Data?) -> Void) {
         session.dataTask(with: url) {
@@ -16,12 +30,11 @@ class NetworkService {
     }
     
     func makePostRequest(page: PostRequests, params: [URLQueryItem] = []) {
-        var url = mainUrl
-        url?.appendPathComponent(page.rawValue)
-        var request: URLComponents = URLComponents(url: url!, resolvingAgainstBaseURL: false)!
-        for par in params {
-            request.queryItems?.append(par)
-        }
+        var request = mainUrl
+        request.path.append(page.rawValue)
+        request.queryItems = []
+        request.queryItems!.append(profileId)
+        request.queryItems!.append(contentsOf: params)
         
         session.dataTask(with: request.url!) { (data, response, error) in
             if let response = response { print(response) }
