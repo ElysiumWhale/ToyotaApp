47167dc3cf8fdd33a51e89a100eac682d3f237bd
Alex Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2020-12-06 01:03:53 +0400

    UI & TMPVC Small update

--- ToyotaApp/MainMenu/TMPVC.swift
+++ ToyotaApp/MainMenu/TMPVC.swift
@@ -2,21 +2,40 @@ import UIKit
 
 class TMPVC: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
-    @IBAction func logout(with sender: Any?) {
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//    }
+    //@IBAction func logout(with sender: Any?) {
         //show dialog
         //send info about logout to server
         //NetworkService.shared.logout()
-        loadAuthScreen()
-    }
-
-    func loadAuthScreen() {
-        let storyBoard: UIStoryboard = UIStoryboard(name: AppStoryboards.auth, bundle: nil)
-        let vc = storyBoard.instantiateViewController(withIdentifier: AppViewControllers.authNavigation)
-        (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(vc)
-    }
+        //loadAuthScreen()
+    //}
+//    func loadAuthScreen() {
+//        let storyBoard: UIStoryboard = UIStoryboard(name: AppStoryboards.auth, bundle: nil)
+//        let vc = storyBoard.instantiateViewController(withIdentifier: AppViewControllers.authNavigation)
+//        (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.changeRootViewController(vc)
+//    }
+    
+//MARK: - UICollectionViewDataSource
+    //@IBOutlet private(set) var carsList: UICollectionView!
+    //    extension AddingCarViewController: UICollectionViewDataSource {
+    //        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section:   Int) -> Int {
+    //            return cars?.count ?? 0
+    //        }
+    //
+    //        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath)   -> UICollectionViewCell {
+    //            let item = cars![indexPath.row]
+    //            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for:   indexPath) as! CarChoosingCell
+    //            cell.configureCell(car: item, showCheckView: cellButtonAction)
+    //            return cell
+    //        }
+    //
+    //        private func cellButtonAction(sender: UICollectionViewCell) {
+    //            DispatchQueue.main.async { [self] in
+    //                performSegue(withIdentifier: checkCarSegueCode, sender: sender)
+    //            }
+    //        }
+    //    }
 
 }
