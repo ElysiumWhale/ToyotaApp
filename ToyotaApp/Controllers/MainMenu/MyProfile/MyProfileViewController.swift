0c65a7f47d24ab7d5f8a13cb1b1408190359d10c
Alex Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2021-03-25 03:02:07 +0400

    Big logic of MyProfileVC Rework

--- ToyotaApp/Controllers/MainMenu/MyProfile/MyProfileViewController.swift
+++ ToyotaApp/Controllers/MainMenu/MyProfile/MyProfileViewController.swift
@@ -1,65 +1,147 @@
 import UIKit
 import SwiftEntryKit
 
+fileprivate enum EditingStates {
+    case none
+    case isEditing
+    case isLoading
+}
+
 class MyProfileViewController: UIViewController {
-    @IBOutlet private(set) var firstNameTextField: UITextField!
-    @IBOutlet private(set) var secondNameTextField: UITextField!
-    @IBOutlet private(set) var lastNameTextField: UITextField!
-    @IBOutlet private(set) var birthTextField: UITextField!
-    @IBOutlet private(set) var emailTextField: UITextField!
+    @IBOutlet private var firstNameTextField: UITextField!
+    @IBOutlet private var secondNameTextField: UITextField!
+    @IBOutlet private var lastNameTextField: UITextField!
+    @IBOutlet private var birthTextField: UITextField!
+    @IBOutlet private var emailTextField: UITextField!
     
-    @IBOutlet private(set) var cancelButton: UIButton!
-    @IBOutlet private(set) var saveButton: UIButton!
+    @IBOutlet private var cancelButton: UIButton!
+    @IBOutlet private var saveButton: UIButton!
     
     private let datePicker: UIDatePicker = UIDatePicker()
-    private var date: String = ""
+    private var date: String = "" {
+        didSet { view.endEditing(true) }
+    }
     
     private let myCarsSegueCode = SegueIdentifiers.MyProfileToCars
     private let settingsSegueCode = SegueIdentifiers.MyProfileToSettings
     
     private var user: UserProxy!
     private var profile: Person { user.getPerson }
-    private var isPersonEditing: Bool = false
+    
+    private var state: EditingStates = .none {
+        didSet { switchInterface(state) }
+    }
+    
+    private func switchInterface(_ state: EditingStates) {
+        let isEditing = state == .isEditing
+        for field in textFieldsWithError.keys {
+            field.isEnabled = isEditing ? true : false
+        }
+        cancelButton.isEnabled = isEditing ? true : false
+        if state != .isLoading {
+            cancelButton.isHidden = !isEditing
+            saveButton.setTitle(isEditing ? "Сохранить" : "Редактировать", for: .normal)
+        }
+    }
+    
+    private var textFieldsWithError: [UITextField : Bool]!
+    
+    private var hasChanges: Bool {
+        profile.firstName != firstNameTextField.text ||
+        profile.secondName != secondNameTextField.text ||
+        profile.lastName != lastNameTextField.text ||
+        profile.email != emailTextField.text ||
+        profile.birthday != birthTextField.text
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         hideKeyboardWhenTappedAround()
-        let person = user.getPerson
-        firstNameTextField.text = person.firstName
-        secondNameTextField.text = person.secondName
-        lastNameTextField.text = person.lastName
-        birthTextField.text = person.birthday
-        emailTextField.text = person.email
+        configureDatePicker(datePicker, with: #selector(dateDidSelect), for: birthTextField)
+        updateFields()
+        textFieldsWithError = [firstNameTextField : false, secondNameTextField : false,
+                               lastNameTextField : false, emailTextField : false, birthTextField : false]
+    }
+    
+    func updateFields() {
+        firstNameTextField.text = profile.firstName
+        secondNameTextField.text = profile.secondName
+        lastNameTextField.text = profile.lastName
+        #warning("to-do: format data")
+        birthTextField.text = profile.birthday
+        emailTextField.text = profile.email
+    }
+    
+    @IBAction func textDidChange(sender: UITextField) {
+        if let text = sender.text, text.count > 0, text.count < 25 {
+            sender.layer.borderColor = UIColor.gray.cgColor
+            sender.layer.borderWidth = 0.15
+            textFieldsWithError[sender] = false
+        } else {
+            sender.layer.borderColor = UIColor.systemRed.cgColor
+            sender.layer.borderWidth = 0.5
+            textFieldsWithError[sender] = true
+        }
+    }
+    
+    func buildQueryParams() -> [URLQueryItem] {
+        var items: [URLQueryItem] = [URLQueryItem(name: RequestKeys.Auth.userId, value: user.getId)]
+        items.append(URLQueryItem(name: RequestKeys.PersonalInfo.firstName, value: firstNameTextField.text))
+        items.append(URLQueryItem(name: RequestKeys.PersonalInfo.secondName, value: secondNameTextField.text))
+        items.append(URLQueryItem(name: RequestKeys.PersonalInfo.lastName, value: lastNameTextField.text))
+        items.append(URLQueryItem(name: RequestKeys.PersonalInfo.email, value: emailTextField.text))
+        items.append(URLQueryItem(name: RequestKeys.PersonalInfo.birthday, value: birthTextField.text))
+        return items
     }
     
     @IBAction func enterEditMode(sender: UIButton) {
-        isPersonEditing = !isPersonEditing
-        if isPersonEditing {
-            saveButton.setTitle("Сохранить", for: .normal)
-        } else {
-            #warning("to-do: save profile query + push to user defaults")
-            saveButton.setTitle("Редактировать", for: .normal)
+        switch state {
+            case .none: state = .isEditing
+            case .isLoading: return
+            case .isEditing: updateUserInfo()
         }
-        switchEditingMode(with: isPersonEditing)
+    }
+    
+    func updateUserInfo() {
+        guard hasChanges else {
+            state = .none
+            return
+        }
+        
+        guard textFieldsWithError.allSatisfy({ !$0.value }) else {
+            PopUp.displayMessage(with: "Неккоректные данные", description: "Проверьте введенную информацию!", buttonText: "Ок")
+            return
+        }
+        
+        state = .isLoading
+        NetworkService.shared.makePostRequest(page: RequestPath.Profile.editProfile, params: buildQueryParams(), completion: completion)
+    }
+    
+    func completion(response: Response?) {
+        DispatchQueue.main.async { [self] in
+            if let success = response, success.errorCode == nil, success.result == "ok" {
+                user.update(Person(firstName: firstNameTextField.text!, lastName: lastNameTextField.text!, secondName: secondNameTextField.text!, email: emailTextField.text!, birthday: birthTextField.text!))
+                PopUp.displayMessage(with: "Успех", description: "Личная информация успешно обновлена", buttonText: "Ок")
+                state = .none
+            } else {
+                PopUp.displayMessage(with: "Ошибка", description: "Произошла ошибка при сохранении данных, повторите попытку позже", buttonText: "Ок")
+                state = .isEditing
+            }
+        }
     }
     
     @IBAction func cancelEdit(sender: UIButton) {
-        saveButton.setTitle("Редактировать", for: .normal)
-        isPersonEditing = false
-        switchEditingMode(with: isPersonEditing)
+        if hasChanges {
+            updateFields()
+            for textField in textFieldsWithError.keys {
+                textDidChange(sender: textField)
+            }
+        }
+        state = .none
     }
     
-    private func switchEditingMode(with: Bool) {
-        firstNameTextField.isEnabled = with
-        secondNameTextField.isEnabled = with
-        lastNameTextField.isEnabled = with
-        birthTextField.isEnabled = with
-        emailTextField.isEnabled = with
-        cancelButton.isEnabled = with
-        cancelButton.isHidden = !with
     @IBAction func dateDidSelect(sender: Any?) {
         date = formatSelectedDate(from: datePicker, to: birthTextField)
-        view.endEditing(true)
     }
     
     @IBAction func logout(sender: Any?) {
