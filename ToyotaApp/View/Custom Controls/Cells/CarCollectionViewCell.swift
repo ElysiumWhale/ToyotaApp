9ad36016e73b5095217a2b32612936e913d3a096
Alex Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2021-04-03 03:42:41 +0400

    Separated storyboard & NEW CAR CELLS

--- ToyotaApp/View/Custom Controls/Cells/CarCollectionViewCell.swift
+++ ToyotaApp/View/Custom Controls/Cells/CarCollectionViewCell.swift
@@ -13,9 +13,16 @@ import UIKit
     
     func configure(brand: String, model: String, color: String, plate: String, colorDesription: String, vin: String) {
         brandNameLabel.text = "\(brand) \(model)"
-        colorNameLabel.text = color
-        liscencePlateLabel.text = plate
-        colorDesrLabel.text = colorDesription
-        vinTextLabel.text = vin
+        colorNameLabel.text = "Цвет: \(color)"
+        liscencePlateLabel.text = plate.uppercased()
+        colorDesrLabel.text = "Описание цвета: \(colorDesription)"
+        vinTextLabel.text =  "VIN: \(vin.map { _ in "*" }.joined())"
+        
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        layer.shadowRadius = 3.5
+        layer.shadowOpacity = 0.7
+        layer.masksToBounds = false
+        layer.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius).cgPath
     }
 }
