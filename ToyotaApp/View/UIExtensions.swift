ce1db0275efa15f77fdff32fbedea0036200bde8
Alex Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2021-07-08 01:06:15 +0400

    fadeIn() & fadeOut() main thread fix

--- ToyotaApp/View/UIExtensions.swift
+++ ToyotaApp/View/UIExtensions.swift
@@ -4,24 +4,30 @@ import UIKit
 //MARK: - FadeIn/Out UIView Animation
 extension UIView {
     func fadeIn(_ duration: TimeInterval? = 0.05, onCompletion: (() -> Void)? = nil) {
-        alpha = 0
-        isHidden = false
-        UIView.animate(withDuration: duration!,
-                       animations: { self.alpha = 1 },
-                       completion: { (value: Bool) in
-                          if let complete = onCompletion { complete() }
-                       }
-        )
+        DispatchQueue.main.async { [weak self] in
+            if let view = self, !view.isHidden { return }
+            self?.alpha = 0
+            self?.isHidden = false
+            UIView.animate(withDuration: duration!,
+                           animations: { self?.alpha = 1 },
+                           completion: { (value: Bool) in
+                              if let complete = onCompletion { complete() }
+                           }
+            )
+        }
     }
 
     func fadeOut(_ duration: TimeInterval? = 0.05, onCompletion: (() -> Void)? = nil) {
-        UIView.animate(withDuration: duration!,
-                       animations: { self.alpha = 0 },
-                       completion: { (value: Bool) in
-                           self.isHidden = true
-                           if let complete = onCompletion { complete() }
-                       }
-        )
+        DispatchQueue.main.async { [weak self] in
+            if let view = self, view.isHidden { return }
+            UIView.animate(withDuration: duration!,
+                           animations: { self?.alpha = 0 },
+                           completion: { (value: Bool) in
+                            self?.isHidden = true
+                            if let complete = onCompletion { complete() }
+                           }
+            )
+        }
     }
 }
 
