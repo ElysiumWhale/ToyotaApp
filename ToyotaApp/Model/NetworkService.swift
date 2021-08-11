9f1f9507ea9b70f3762ed1d00f9ab4332d9ac7d7
Alex Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2021-08-12 02:05:55 +0400

    Debug response print rework

--- ToyotaApp/Model/NetworkService.swift
+++ ToyotaApp/Model/NetworkService.swift
@@ -16,7 +16,11 @@ class NetworkService {
         let request = buildPostRequest(for: page, with: params)
         
         session.dataTask(with: request) { (data, response, error) in
-            if let response = response { print(response) }
+            if let response = response as? HTTPURLResponse {
+                #if DEBUG
+                print("\n", "Request status code: ", response.statusCode, "\n")
+                #endif
+            }
             if error != nil {
                 #warning("todo: switch by error code")
                 completion(Result.failure(ErrorResponse(code: NetworkErrors.lostConnection.rawValue, message: AppErrors.connectionLost.rawValue)))
