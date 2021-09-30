ccda35450f59885e29ef6a4956fc3f68b8d64aed
Alexey Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2021-10-01 00:19:19 +0400

    Modules logic separation

--- ToyotaApp/Controllers/MainMenu/Service/Modules/MapModule.swift
+++ ToyotaApp/Controllers/MainMenu/Service/Modules/MapModule.swift
@@ -1,66 +1,6 @@
 import UIKit
 import MapKit
 
-// MARK: - View
-class MapModuleView: UIView {
-    private(set) lazy var label: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.toyotaType(.semibold, of: 20)
-        label.textColor = .label
-        label.textAlignment = .left
-        label.text = .common(.enterLocation)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-
-    private(set) lazy var map: MKMapView = {
-        let map = MKMapView()
-        map.mapType = .hybrid
-        map.layer.cornerRadius = 15
-        map.isZoomEnabled = true
-        map.isScrollEnabled = true
-        map.isRotateEnabled = true
-        map.isUserInteractionEnabled = true
-        map.showsCompass = true
-        map.translatesAutoresizingMaskIntoConstraints = false
-        return map
-    }()
-
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        configureSubviews()
-    }
-
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        configureSubviews()
-    }
-
-    private func configureSubviews() {
-        addSubview(label)
-        addSubview(map)
-        setupLayout()
-    }
-
-    private func setupLayout() {
-        NSLayoutConstraint.activate([
-            label.topAnchor.constraint(equalTo: topAnchor),
-            label.leadingAnchor.constraint(equalTo: leadingAnchor),
-            label.trailingAnchor.constraint(equalTo: trailingAnchor),
-            map.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 10),
-            map.leadingAnchor.constraint(equalTo: leadingAnchor),
-            map.trailingAnchor.constraint(equalTo: trailingAnchor),
-            map.bottomAnchor.constraint(equalTo: bottomAnchor),
-            map.heightAnchor.constraint(equalToConstant: 500)
-        ])
-    }
-
-    override class var requiresConstraintBasedLayout: Bool {
-        return true
-    }
-}
-
-// MARK: - Module
 class MapModule: NSObject, IServiceModule {
     var view: UIView? { internalView }
 
@@ -82,7 +22,7 @@ class MapModule: NSObject, IServiceModule {
         }
     }
 
-    internal weak var delegate: IServiceController?
+    weak var delegate: IServiceController?
 
     init(with type: ServiceType) {
         serviceType = type
--- ToyotaApp/Controllers/MainMenu/Service/Modules/TimePickerModule.swift
+++ ToyotaApp/Controllers/MainMenu/Service/Modules/TimePickerModule.swift
@@ -5,68 +5,6 @@ private struct FreeTime {
     let freeTime: [DateComponents]
 }
 
-// MARK: - View
-class TimePickerView: UIView {
-    private(set) lazy var datePicker: UIPickerView = {
-        let picker = UIPickerView()
-        picker.translatesAutoresizingMaskIntoConstraints = false
-        return picker
-    }()
-
-    private(set) lazy var dateTimeLabel: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.toyotaType(.semibold, of: 20)
-        label.textAlignment = .left
-        label.textColor = .label
-        label.text = .common(.chooseDateTime)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        configureSubviews()
-    }
-
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        configureSubviews()
-    }
-
-    private func configureSubviews() {
-        addSubview(dateTimeLabel)
-        addSubview(datePicker)
-        setupLayout()
-    }
-
-    private func setupLayout() {
-        NSLayoutConstraint.activate([
-            dateTimeLabel.topAnchor.constraint(equalTo: topAnchor),
-            dateTimeLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
-            dateTimeLabel.leadingAnchor.constraint(equalTo: trailingAnchor),
-            datePicker.bottomAnchor.constraint(equalTo: bottomAnchor),
-            datePicker.leadingAnchor.constraint(equalTo: leadingAnchor),
-            datePicker.leadingAnchor.constraint(equalTo: trailingAnchor),
-            datePicker.widthAnchor.constraint(equalTo: widthAnchor, constant: 0),
-            datePicker.heightAnchor.constraint(equalToConstant: 150),
-            dateTimeLabel.bottomAnchor.constraint(equalTo: datePicker.topAnchor, constant: -5)
-        ])
-    }
-
-    override class var requiresConstraintBasedLayout: Bool {
-        return true
-    }
-
-    func dataDidDownload() {
-        DispatchQueue.main.async { [weak self] in
-            self?.datePicker.selectRow(0, inComponent: 0, animated: false)
-            self?.datePicker.reloadAllComponents()
-            self?.fadeIn()
-        }
-    }
-}
-
-// MARK: - Module
 class TimePickerModule: NSObject, IServiceModule {
     var view: UIView? { internalView }
 
@@ -96,7 +34,7 @@ class TimePickerModule: NSObject, IServiceModule {
         }
     }
 
-    internal weak var delegate: IServiceController?
+    weak var delegate: IServiceController?
 
     init(with type: ServiceType) {
         serviceType = type
