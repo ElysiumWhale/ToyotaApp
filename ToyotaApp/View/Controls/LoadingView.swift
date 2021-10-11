e3c14b8cc5a5ddb9bf2b434790163a13c2171701
Alexey Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2021-10-12 00:43:07 +0400

    LoadingView Add

---
+++ ToyotaApp/View/Controls/LoadingView.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+class LoadingView: UIView {
+
+    private(set) var indicator = UIActivityIndicatorView(style: .large)
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func configure() {
+        addSubview(indicator)
+        indicator.translatesAutoresizingMaskIntoConstraints = false
+        indicator.tintColor = .white
+        backgroundColor = .appTint(.loading)
+        NSLayoutConstraint.activate([
+            indicator.centerXAnchor.constraint(equalTo: centerXAnchor),
+            indicator.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+
+    func startAnimating() {
+        indicator.startAnimating()
+    }
+
+    func stopAnimating() {
+        indicator.stopAnimating()
+    }
+}
