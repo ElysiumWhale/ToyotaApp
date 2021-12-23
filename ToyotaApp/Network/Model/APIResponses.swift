b7545b5cc421982e92ad1ef106d1f4803bc6d927
Alexey Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2021-12-24 02:36:54 +0400

    APIResponse and Models Separation

--- ToyotaApp/Network/Model/APIResponses.swift
+++ ToyotaApp/Network/Model/APIResponses.swift
@@ -5,7 +5,7 @@ protocol IResponse: Codable {
 }
 
 // MARK: - Default response
-public struct Response: IResponse {
+struct Response: IResponse {
     let result: String
 
     private enum CodingKeys: String, CodingKey {
@@ -14,7 +14,7 @@ public struct Response: IResponse {
 }
 
 // MARK: - CheckUserOrSmsCodeResponse
-public struct CheckUserOrSmsCodeResponse: IResponse {
+struct CheckUserOrSmsCodeResponse: IResponse {
     let result: String
     let secretKey: String
     let userId: String?
@@ -34,54 +34,8 @@ public struct CheckUserOrSmsCodeResponse: IResponse {
     }
 }
 
-public struct RegisteredUser: IResponse {
-    let profile: Profile
-    let cars: [Car]?
-}
-
-public struct Profile: IResponse {
-    let phone: String?
-    let firstName: String?
-    let lastName: String?
-    let secondName: String?
-    let email: String?
-    let birthday: String?
-
-    private enum CodingKeys: String, CodingKey {
-        case phone, email, birthday
-        case firstName = "first_name"
-        case lastName = "last_name"
-        case secondName = "second_name"
-    }
-}
-
-struct Car: IService {
-    let id: String
-    let brand: String
-    let model: Model
-    let color: Color
-    let year: String
-    let plate: String
-    let vin: String
-    let isChecked: Bool?
-
-    var name: String {
-        "\(brand) \(model.name)"
-    }
-
-    private enum CodingKeys: String, CodingKey {
-        case model, color
-        case id = "car_id"
-        case brand = "car_brand_name"
-        case year = "car_year"
-        case plate = "license_plate"
-        case vin = "vin_code"
-        case isChecked
-    }
-}
-
 // MARK: - CitiesResponse
-public struct CitiesResponse: IServiceResponse {
+struct CitiesResponse: IServiceResponse {
     let result: String
     let cities: [City]
     let models: [Model]?
@@ -90,63 +44,21 @@ public struct CitiesResponse: IServiceResponse {
     var array: [IService] { cities }
 }
 
-public struct City: IService, WithDefaultKey {
-    static var key: DefaultKeys = .selectedCity
-
-    let id: String
-    let name: String
-
-    private enum CodingKeys: String, CodingKey {
-        case id
-        case name = "city_name"
-    }
-}
-
-public struct Model: Codable {
-    let id: String
-    let name: String
-    let brandId: String
-
-    private enum CodingKeys: String, CodingKey {
-        case id
-        case name = "car_model_name"
-        case brandId = "car_brand_id"
-    }
-}
-
-public struct Color: Codable {
-    let id: String
-    let name: String
-    let code: String
-    let colorDescription: String
-    let isMetallic: String
-    let hex: String
-
-    private enum CodingKeys: String, CodingKey {
-        case id
-        case name = "car_color_name"
-        case code = "color_code"
-        case colorDescription = "color_description"
-        case isMetallic = "color_metallic"
-        case hex = "color_swatch"
-    }
-}
-
 // MARK: - ShowroomsResponse
-public struct ShowroomsResponse: IResponse {
+struct ShowroomsResponse: IResponse {
     let result: String
     let showrooms: [Showroom]
 }
 
 // MARK: - ModelsAndColorsResponse
-public struct ModelsAndColorsResponse: IResponse {
+struct ModelsAndColorsResponse: IResponse {
     let result: String
     let models: [Model]
     let colors: [Color]
 }
 
 // MARK: - CarSetResponse
-public struct CarSetResponse: IResponse {
+struct CarSetResponse: IResponse {
     let result: String
     let carId: String
 
@@ -157,22 +69,15 @@ public struct CarSetResponse: IResponse {
 }
 
 // MARK: - ShoroomsResponce
-public struct ShoroomsResponce: IServiceResponse {
+struct ShoroomsResponce: IServiceResponse {
     let result: String
     let showrooms: [Showroom]
 
     var array: [IService] { showrooms }
 }
 
-// MARK: - CarCheckResponse
-public struct CarCheckResponse: IResponse {
-    let result: String
-    let message: String // todo: delete message
-    // let car: OldCar?
-}
-
 // MARK: - ServicesTypesResponse
-public struct ServicesTypesResponse: IResponse {
+struct ServicesTypesResponse: IResponse {
     let result: String
     let serviceType: [ServiceType]
 
@@ -182,44 +87,16 @@ public struct ServicesTypesResponse: IResponse {
     }
 }
 
-public struct ServiceType: Codable {
-    let id: String
-    let serviceTypeName: String
-    let controlTypeId: String
-    let controlTypeDesc: String
-
-    private enum CodingKeys: String, CodingKey {
-        case id
-        case serviceTypeName = "service_type_name"
-        case controlTypeId = "control_type_id"
-        case controlTypeDesc = "control_type_desc"
-    }
-}
-
 // MARK: - ServicesResponse
-public struct ServicesResponse: IServiceResponse {
+struct ServicesResponse: IServiceResponse {
     let result: String
     let services: [Service]
 
     var array: [IService] { services }
 }
 
-public struct Service: IService {
-    let id: String
-    let name: String
-
-    private enum CodingKeys: String, CodingKey {
-        case id
-        case name = "service_name"
-    }
-}
-
-extension Service {
-    static let empty = Service(id: "-1", name: "Нет доступных сервисов")
-}
-
 // MARK: - FreeTimeResponse
-public struct FreeTimeResponse: IResponse {
+struct FreeTimeResponse: IResponse {
     let result: String
     let startDate: String?
     let endDate: String?
@@ -234,35 +111,13 @@ public struct FreeTimeResponse: IResponse {
 }
 
 // MARK: - ManagersResponse
-public struct ManagersResponse: IResponse {
+struct ManagersResponse: IResponse {
     let result: String
     let managers: [Manager]
 }
 
-public struct Manager: Codable {
-    let id: String
-    let userId: String
-    let firstName: String
-    let secondName: String
-    let lastName: String
-    let phone: String
-    let email: String
-    let imageUrl: String
-    let showroomName: String
-
-    private enum CodingKeys: String, CodingKey {
-        case id, phone, email
-        case userId = "user_id"
-        case firstName = "first_name"
-        case secondName = "second_name"
-        case lastName = "last_name"
-        case imageUrl = "avatar"
-        case showroomName = "showroom_name"
-    }
-}
-
 // MARK: - CarsResponse
-public struct CarsResponse: IServiceResponse {
+struct CarsResponse: IServiceResponse {
     var array: [IService] { cars }
 
     let result: String
@@ -270,43 +125,8 @@ public struct CarsResponse: IServiceResponse {
 }
 
 // MARK: - BookingsResponse
-public struct BookingsResponse: IResponse {
+struct BookingsResponse: IResponse {
     let result: String
     let booking: [Booking]
     let count: Int
-}
-
-public struct Booking: Codable {
-    let date: String
-    let startTime: String
-    let latitude: String
-    let longitude: String
-    let status: BookingStatus
-    let carName: String
-    let licensePlate: String
-    let showroomName: String
-    let serviceName: String
-    let postName: String
-
-    private enum CodingKeys: String, CodingKey {
-        case latitude, longitude, status
-        case date = "date_booking"
-        case startTime = "booking_start_time"
-        case carName = "car_model_name"
-        case licensePlate = "license_plate"
-        case showroomName = "showroom_name"
-        case serviceName = "service_name"
-        case postName = "post_name"
-    }
-}
-
-/// Temporary struct
-public struct News {
-    let title: String
-    let content: String = .empty
-    let date: Date = Date()
-    let showroomId: String = .empty
-    let showroomName: String = .empty
-    let imgUrl: URL?
-    let url: URL?
 }
---
+++ ToyotaApp/Network/Model/Models.swift
@@ -0,0 +1,189 @@
+import Foundation
+
+// MARK: - RegisteredUser
+struct RegisteredUser: Codable {
+    let profile: Profile
+    let cars: [Car]?
+}
+
+// MARK: - Profile
+struct Profile: Codable {
+    let phone: String?
+    let firstName: String?
+    let lastName: String?
+    let secondName: String?
+    let email: String?
+    let birthday: String?
+
+    private enum CodingKeys: String, CodingKey {
+        case phone, email, birthday
+        case firstName = "first_name"
+        case lastName = "last_name"
+        case secondName = "second_name"
+    }
+}
+
+// MARK: - Car
+struct Car: IService {
+    let id: String
+    let brand: String
+    let model: Model
+    let color: Color
+    let year: String
+    let plate: String
+    let vin: String
+    let isChecked: Bool?
+
+    var name: String {
+        "\(brand) \(model.name)"
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case model, color
+        case id = "car_id"
+        case brand = "car_brand_name"
+        case year = "car_year"
+        case plate = "license_plate"
+        case vin = "vin_code"
+        case isChecked
+    }
+}
+
+// MARK: - City
+struct City: IService, WithDefaultKey {
+    static var key: DefaultKeys = .selectedCity
+
+    let id: String
+    let name: String
+
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case name = "city_name"
+    }
+}
+
+// MARK: - Model
+struct Model: IService {
+    let id: String
+    let name: String
+    let brandId: String
+
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case name = "car_model_name"
+        case brandId = "car_brand_id"
+    }
+}
+
+// MARK: - Color
+struct Color: IService {
+    let id: String
+    let name: String
+    let code: String
+    let colorDescription: String
+    let isMetallic: String
+    let hex: String
+
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case name = "car_color_name"
+        case code = "color_code"
+        case colorDescription = "color_description"
+        case isMetallic = "color_metallic"
+        case hex = "color_swatch"
+    }
+}
+
+// MARK: - ServiceType
+struct ServiceType: IService {
+    let id: String
+    let serviceTypeName: String
+    let controlTypeId: String
+    let controlTypeDesc: String
+
+    var name: String {
+        serviceTypeName
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case serviceTypeName = "service_type_name"
+        case controlTypeId = "control_type_id"
+        case controlTypeDesc = "control_type_desc"
+    }
+}
+
+// MARK: - Service
+struct Service: IService {
+    let id: String
+    let name: String
+
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case name = "service_name"
+    }
+}
+
+extension Service {
+    static let empty = Service(id: "-1", name: "Нет доступных сервисов")
+}
+
+// MARK: - Manager
+struct Manager: Codable {
+    let id: String
+    let userId: String
+    let firstName: String
+    let secondName: String
+    let lastName: String
+    let phone: String
+    let email: String
+    let imageUrl: String
+    let showroomName: String
+
+    private enum CodingKeys: String, CodingKey {
+        case id, phone, email
+        case userId = "user_id"
+        case firstName = "first_name"
+        case secondName = "second_name"
+        case lastName = "last_name"
+        case imageUrl = "avatar"
+        case showroomName = "showroom_name"
+    }
+}
+
+// MARK: - Booking
+struct Booking: Codable {
+    let date: String
+    let startTime: String
+    let latitude: String
+    let longitude: String
+    let status: BookingStatus
+    let carName: String
+    let licensePlate: String
+    let showroomName: String
+    let serviceName: String
+    let postName: String
+
+    private enum CodingKeys: String, CodingKey {
+        case latitude, longitude, status
+        case date = "date_booking"
+        case startTime = "booking_start_time"
+        case carName = "car_model_name"
+        case licensePlate = "license_plate"
+        case showroomName = "showroom_name"
+        case serviceName = "service_name"
+        case postName = "post_name"
+    }
+}
+
+// MARK: - News (Temporary)
+/// Temporary struct
+struct News {
+    let title: String
+    let content: String = .empty
+    let date: Date = Date()
+    let showroomId: String = .empty
+    let showroomName: String = .empty
+    let imgUrl: URL?
+    let url: URL?
+}
