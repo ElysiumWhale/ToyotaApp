c5f962f19a4f3a2ad408d286a14c2178fcdd2ccc
Alexey Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2021-12-26 19:28:11 +0400

    TintedImageButton & Storyboard fixed

--- ToyotaApp/Views/Controls/Button/TintedImageButton.swift
+++ ToyotaApp/Views/Controls/Button/TintedImageButton.swift
@@ -4,17 +4,25 @@ import UIKit
 class TintedImageButton: CustomizableButton {
 
     @IBInspectable
-    var highlightedTintColor: UIColor = .clear
+    var highlightedTintColor: UIColor = .clear {
+        didSet {
+            setTitleColor(highlightedTintColor, for: .highlighted)
+        }
+    }
 
     @IBInspectable
     var normalTintColor: UIColor = .clear {
         didSet {
             imageView?.tintColor = normalTintColor
+            setTitleColor(normalTintColor, for: .normal)
         }
     }
 
     override var isHighlighted: Bool {
         didSet {
+            backgroundColor = isHighlighted
+                ? highlightedColor
+                : normalColor
             imageView?.tintColor = isHighlighted
                 ? highlightedTintColor
                 : normalTintColor
