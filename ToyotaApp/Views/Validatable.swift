178addd12998c4fbd3ecaa030f22af390ec5604e
Alexey Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2022-01-16 03:43:12 +0400

    Added Validatable

---
+++ ToyotaApp/Views/Validatable.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+protocol Validatable {
+    var rule: ValidationRule? { get set }
+    var isValid: Bool { get }
+    var validationEnabled: Bool { get set }
+    func isValid(for rule: ValidationRule) -> Bool
+}
+
+class ValidationRule {
+    let validationClouse: (String?) -> Bool
+
+    func validate(text: String?) -> Bool {
+        validationClouse(text)
+    }
+
+    init(_ closure: @escaping (String?) -> Bool = { _ in true }) {
+        self.validationClouse = closure
+    }
+}
+
+extension ValidationRule {
+    static let personalInfo = ValidationRule { text in
+        text != nil && !text!.isEmpty && text!.count < 25
+    }
+}
