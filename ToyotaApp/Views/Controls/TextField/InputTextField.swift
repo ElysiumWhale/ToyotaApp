e74c2c8fc6c1b0ef63a826dd05249cbe36cc7e55
Alexey Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2022-01-16 04:13:07 +0400

    Reworked InputTextField
    - implemented Validatable
    - self validating strategy
    - safe inputText property (not only main thread)

--- ToyotaApp/Views/Controls/TextField/InputTextField.swift
+++ ToyotaApp/Views/Controls/TextField/InputTextField.swift
@@ -1,7 +1,8 @@
 import UIKit
 
 @IBDesignable
-class InputTextField: UITextField {
+class InputTextField: UITextField, Validatable {
+    // MARK: - Inspectables
     @IBInspectable
     var cornerRadius: CGFloat {
         get {
@@ -13,30 +14,70 @@ class InputTextField: UITextField {
     }
 
     @IBInspectable
-    var leftPadding: CGFloat = 5 {
+    var leftPadding: CGFloat = 5
+
+    @IBInspectable
+    var maxSymbolCount: Int = 50
+
+    // MARK: - Properties
+    var rule: ValidationRule?
+    var validationEnabled: Bool = true
+
+    private(set) var isValid: Bool = true
+    /// Safe `text` property for using **not only** on `Main` thread
+    private(set) var inputText: String = .empty
+
+    override var text: String? {
         didSet {
-            // todo
+            inputDidChanged(sender: self)
         }
     }
 
-    override func prepareForInterfaceBuilder() {
-        customizeView()
+    // MARK: - Methods
+    func isValid(for rule: ValidationRule) -> Bool {
+        rule.validate(text: inputText)
     }
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        customizeView()
+    func validate(newText: String?) {
+        inputText = newText ?? .empty
+        guard validationEnabled, let rule = rule else {
+            return
+        }
+
+        isValid = isValid(for: rule)
+        toggle(state: isValid ? .normal : .error)
     }
 
-    func customizeView() {
-        layer.cornerRadius = cornerRadius
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        addTarget(self, action: #selector(inputDidChanged), for: .editingChanged)
     }
 
     override func textRect(forBounds bounds: CGRect) -> CGRect {
-        super.textRect(forBounds: bounds).inset(by: UIEdgeInsets(top: 0, left: leftPadding, bottom: 0, right: 0))
+        super.textRect(forBounds: bounds).inset(by: withPaddingInset)
     }
 
     override func editingRect(forBounds bounds: CGRect) -> CGRect {
-        super.editingRect(forBounds: bounds).inset(by: UIEdgeInsets(top: 0, left: leftPadding, bottom: 0, right: 0))
+        super.editingRect(forBounds: bounds).inset(by: withPaddingInset)
+    }
+}
+
+extension InputTextField {
+    var withPaddingInset: UIEdgeInsets {
+        UIEdgeInsets(top: 0, left: leftPadding, bottom: 0, right: 0)
+    }
+
+    @objc private func inputDidChanged(sender: UITextField?) {
+        guard self === sender else {
+            return
+        }
+
+        if let text = text, text.count >= maxSymbolCount {
+            self.text = inputText
+            return
+        }
+
+        validate(newText: text)
     }
 }
