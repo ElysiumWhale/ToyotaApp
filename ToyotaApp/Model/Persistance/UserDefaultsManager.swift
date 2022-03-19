4041c70a29f212014d7f6e067357e8da6cb9a4b5
Alexey Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2022-03-19 19:17:28 +0400

    Add different containers support for DefaultsManager and DefaultsBacked

--- ToyotaApp/Model/Persistance/UserDefaultsManager.swift
+++ ToyotaApp/Model/Persistance/UserDefaultsManager.swift
@@ -8,18 +8,22 @@ enum DefaultKeys: String {
 }
 
 public class DefaultsManager {
-    private static let defaults = UserDefaults.standard
+    class func getUserInfo<T: Codable>(for key: DefaultKeys,
+                                       container: UserDefaults = .standard) -> T? {
+        guard let data = container.data(forKey: key.rawValue) else {
+            return nil
+        }
 
-    class func getUserInfo<T: Codable>(for key: DefaultKeys) -> T? {
-        guard let data = defaults.data(forKey: key.rawValue) else { return nil }
         return try? JSONDecoder().decode(T.self, from: data)
     }
 
     @discardableResult
-    class func push<T: Codable>(info: T, for key: DefaultKeys) -> Bool {
+    class func push<T: Codable>(info: T,
+                                for key: DefaultKeys,
+                                container: UserDefaults = .standard) -> Bool {
         do {
             let data = try JSONEncoder().encode(info)
-            defaults.set(data, forKey: key.rawValue)
+            container.set(data, forKey: key.rawValue)
             return true
         } catch let decodeError as NSError {
             assertionFailure("Encoder error: \(decodeError.localizedDescription)")
@@ -27,9 +31,9 @@ public class DefaultsManager {
         }
     }
 
-    class func clearAll() {
-        defaults.removePersistentDomain(forName: Bundle.main.bundleIdentifier!)
-        defaults.synchronize()
+    class func clearAll(in container: UserDefaults = .standard) {
+        container.removePersistentDomain(forName: Bundle.main.bundleIdentifier!)
+        container.synchronize()
     }
 }
 
@@ -47,9 +51,15 @@ extension Bool {
 // MARK: - DefaultsBacked
 @propertyWrapper struct DefaultsBacked<T: Codable> {
     let key: DefaultKeys
+    let container: UserDefaults
 
     var wrappedValue: T? {
-        get { DefaultsManager.getUserInfo(for: key) }
-        set { DefaultsManager.push(info: newValue, for: key) }
+        get { DefaultsManager.getUserInfo(for: key, container: container) }
+        set { DefaultsManager.push(info: newValue, for: key, container: container) }
+    }
+
+    init(key: DefaultKeys, container: UserDefaults = .standard) {
+        self.key = key
+        self.container = container
     }
 }
