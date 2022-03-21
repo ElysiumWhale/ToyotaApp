1d3cbbebf567138f134cff1ead6758ad6f61162b
Alexey Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2022-03-22 01:45:17 +0400

    Update Keychain manager for testing

--- ToyotaApp/Model/Persistance/KeychainManager.swift
+++ ToyotaApp/Model/Persistance/KeychainManager.swift
@@ -18,35 +18,38 @@ protocol Keychainable: Codable {
 
 /// Utility class for managing data stored in `Keychain`
 class KeychainManager<T: Keychainable> {
-    class func get() -> T? {
-        guard let data = KeychainWrapper.standard.data(forKey: T.key.rawValue) else { return nil }
+    class func get(from wrapper: KeychainWrapper = .standard) -> T? {
+        guard let data = wrapper.data(forKey: T.key.rawValue) else {
+            return nil
+        }
+
         return try? JSONDecoder().decode(T.self, from: data)
     }
 
-    class func set(_ info: T) {
+    class func set(_ info: T, to wrapper: KeychainWrapper = .standard) {
         if let data = try? JSONEncoder().encode(info) {
-            KeychainWrapper.standard.set(data, forKey: T.key.rawValue.rawValue)
+            wrapper.set(data, forKey: T.key.rawValue.rawValue)
         }
     }
 
-    class func update(_ action: (T?) -> T) {
-        guard let data = KeychainWrapper.standard.data(forKey: T.key.rawValue),
+    class func update(_ action: (T?) -> T, in wrapper: KeychainWrapper = .standard) {
+        guard let data = wrapper.data(forKey: T.key.rawValue),
               let value = try? JSONDecoder().decode(T.self, from: data) else {
             set(action(nil))
             return
         }
 
-        set(action(value))
+        set(action(value), to: wrapper)
     }
 
-    class func clear() {
-        KeychainWrapper.standard.remove(forKey: T.key.rawValue)
+    class func clear(from wrapper: KeychainWrapper = .standard) {
+        wrapper.remove(forKey: T.key.rawValue)
     }
 }
 
 // MARK: - T == UserId for avoiding specifying type
 extension KeychainManager where T == UserId {
-    static func clearAll() {
-        KeychainWrapper.standard.removeAllKeys()
+    static func clearAll(from wrapper: KeychainWrapper = .standard) {
+        wrapper.removeAllKeys()
     }
 }
