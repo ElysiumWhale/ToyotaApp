3a3ac1af2efdc3bf90346532f57489ce9cb0d615
Alexey Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2022-03-27 19:03:11 +0400

    Implement new network stack

--- ToyotaApp/Controllers/Registration/PersonalInfoModule/PersonalInfoInteractor.swift
+++ ToyotaApp/Controllers/Registration/PersonalInfoModule/PersonalInfoInteractor.swift
@@ -14,13 +14,15 @@ enum PersonalDataStoreState {
 }
 
 class PersonalInfoInteractor: PersonalInfoDataStore, PersonalInfoControllerOutput {
+    let infoService: InfoService
     let presenter: PersonalInfoPresenter
 
     var state: PersonalDataStoreState
 
-    init(output: PersonalInfoPresenter) {
+    init(output: PersonalInfoPresenter, state: PersonalDataStoreState = .empty, service: InfoService = .init()) {
         presenter = output
-        state = .empty
+        infoService = service
+        self.state = state
     }
 
     private lazy var requestHandler: RequestHandler<CitiesResponse> = {
@@ -36,11 +38,6 @@ class PersonalInfoInteractor: PersonalInfoDataStore, PersonalInfoControllerOutpu
     private var onSavePerson: Closure?
 
     func setPerson(request: PersonalInfoModels.SetPersonRequest) {
-        var params = buildParams(firstName: request.firstName,
-                                 secondName: request.secondName,
-                                 lastName: request.lastName,
-                                 email: request.email,
-                                 date: request.date)
         onSavePerson = { [weak self] in
             KeychainManager.set(Person(firstName: request.firstName,
                                        lastName: request.secondName,
@@ -50,29 +47,11 @@ class PersonalInfoInteractor: PersonalInfoDataStore, PersonalInfoControllerOutpu
             self?.onSavePerson = nil
         }
 
-        NetworkService.makeRequest(page: .registration(.setProfile),
-                                   params: params.withUserId(),
-                                   handler: requestHandler)
+        infoService.setProfile(with: .from(request), handler: requestHandler)
     }
 
     private func handle(success response: CitiesResponse) {
         onSavePerson?()
         presenter.personDidSet(response: .success(response: response))
-    }
-}
-
-// MARK: - Parameters building
-extension PersonalInfoInteractor {
-    private func buildParams(firstName: String?,
-                             secondName: String?,
-                             lastName: String?,
-                             email: String?,
-                             date: String) -> RequestItems {
-        [(.auth(.brandId), Brand.Toyota),
-         ((.personalInfo(.firstName), firstName)),
-         ((.personalInfo(.secondName), secondName)),
-         ((.personalInfo(.lastName), lastName)),
-         ((.personalInfo(.email), email)),
-         ((.personalInfo(.birthday), date))]
     }
 }
