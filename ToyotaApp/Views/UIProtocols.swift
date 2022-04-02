279bd887701129ee6edbe09dbe63beedabf3adca
Alexey Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2022-04-03 02:20:32 +0400

    Delete redundant protocol

--- ToyotaApp/Views/UIProtocols.swift
+++ ToyotaApp/Views/UIProtocols.swift
@@ -32,24 +32,6 @@ enum Storyboards: String {
     case mainMenu = "MainMenu"
 }
 
-protocol Storyboarded {
-    static var storyBoard: Storyboards { get }
-    static func instatinate() -> Self
-    static func instatinate(from storyboard: Storyboards) -> Self
-}
-
-extension Storyboarded where Self: UIViewController {
-    static func instantinate() -> Self {
-        let name = String(describing: self)
-        let viewControllerIdentifier = name.components(separatedBy: ".")[1]
-        let storyboard = UIStoryboard(name: storyBoard.rawValue, bundle: Bundle.main)
-        guard let result = storyboard.instantiateViewController(withIdentifier: viewControllerIdentifier) as? Self else { fatalError() }
-        return result
-    }
-
-    static func instantinate(from storyboard: Storyboards) { }
-}
-
 // MARK: - Identifiable cells
 typealias TableCell = IdentifiableTableCell & UITableViewCell
 
