0ca5c516ac0a07775c37b57cee49e9332ab47682
Alexey Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2022-04-13 00:55:44 +0400

    Add MainTabController and entry point

--- ToyotaApp/Navigation/Flows/MainMenuFlow.swift
+++ ToyotaApp/Navigation/Flows/MainMenuFlow.swift
@@ -1,11 +1,18 @@
 import UIKit
 
+private typealias TabConfiguration = MainTabBarController.TabConfiguration
+
 enum MainMenuFlow {
     static let storyboard: UIStoryboard = UIStoryboard(.mainMenu)
 
     static func entryPoint(with user: UserProxy) -> UIViewController {
         let tbvc: UITabBarController = storyboard.instantiate(.mainMenuTabBar)
         tbvc.setUserForChildren(user)
+        return tbvc
+    }
+
+    static func entryPoint(for user: UserProxy) -> UIViewController {
+        let tbvc = MainTabBarController()
         return tbvc
     }
 }
