04754bcef580abe81cf35045921bf65fc1a41538
Alexey Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2022-04-16 01:55:28 +0400

    Add resources tests

---
+++ ToyotaAppTests/Resources/ResourcesTests.swift
@@ -0,0 +1,22 @@
+import XCTest
+@testable import ToyotaApp
+
+final class FontsTests: XCTestCase {
+    func testToyotaFonts() throws {
+        for type in UIFont.ToyotaFonts.allCases {
+            let font = UIFont(name: type.name, size: 10)
+            print("[TEST] Font name: \(type.name), Value: \(String(describing: font))")
+            XCTAssertNotNil(font)
+        }
+    }
+}
+
+final class TintsTests: XCTestCase {
+    func testAppTints() throws {
+        for tint in UIColor.AppTints.allCases {
+            let color = UIColor(named: tint.rawValue)
+            print("[TEST] Color name: \(tint.rawValue), Value: \(String(describing: color))")
+            XCTAssertNotNil(color)
+        }
+    }
+}
