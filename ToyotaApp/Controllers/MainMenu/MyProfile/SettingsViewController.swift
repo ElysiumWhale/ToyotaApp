0bfe00cc73ea7c1c74acb8d3b2010b3d7316ddf4
Alexey Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2022-08-02 03:17:04 +0400

    Add observation for phoneUpdate to Settings screen

--- ToyotaApp/Controllers/MainMenu/MyProfile/SettingsViewController.swift
+++ ToyotaApp/Controllers/MainMenu/MyProfile/SettingsViewController.swift
@@ -91,6 +91,8 @@ final class SettingsViewController: InitialazableViewController {
     }
 
     override func configureActions() {
+        EventNotificator.shared.add(self, for: .phoneUpdate)
+
         changeNumberButton.addAction { [weak self] in
             self?.changeNumber()
         }
@@ -111,5 +113,18 @@ final class SettingsViewController: InitialazableViewController {
     private func showAgreement() {
         present(UtilsFlow.agreementModule().wrappedInNavigation,
                 animated: true)
+    }
+}
+
+// MARK: - ObservesEvents
+extension SettingsViewController: ObservesEvents {
+    func handle(event: EventNotificator.AppEvents, notificator: EventNotificator) {
+        guard event == .phoneUpdate else {
+            return
+        }
+
+        dispatch { [self] in
+            phoneTextField.text = user.phone
+        }
     }
 }
