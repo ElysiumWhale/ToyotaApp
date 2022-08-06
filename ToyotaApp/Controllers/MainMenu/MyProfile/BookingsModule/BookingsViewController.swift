20d422641a61829bdd0b37f90f277cd15922c2ff
Alexey Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2022-08-06 04:27:03 +0400

    Rework BookingsVC (no build)

---
+++ ToyotaApp/Controllers/MainMenu/MyProfile/BookingsModule/BookingsViewController.swift
@@ -0,0 +1,87 @@
+import UIKit
+
+final class BookingsViewController: BaseViewController, Refreshable, BookingsView {
+    private let interactor: BookingsInteractor
+
+    private let doneItem = UIBarButtonItem()
+
+    let refreshableView: UITableView! = UITableView(frame: .zero, style: .insetGrouped)
+    let refreshControl = UIRefreshControl()
+
+    init(interactor: BookingsInteractor) {
+        self.interactor = interactor
+
+        super.init()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        refreshableView.dataSource = self
+        interactor.view = self
+
+        startRefreshing()
+    }
+
+    override func addViews() {
+        addSubviews(refreshableView)
+        configureRefresh()
+        navigationItem.rightBarButtonItem = doneItem
+        refreshableView.tableFooterView = UIView()
+        refreshableView.registerCell(BookingCell.self)
+    }
+
+    override func configureLayout() {
+        refreshableView.edgesToSuperview()
+    }
+
+    override func configureAppearance() {
+        view.backgroundColor = .systemBackground
+        refreshableView.allowsSelection = false
+
+        doneItem.tintColor = .appTint(.secondarySignatureRed)
+    }
+
+    override func localize() {
+        navigationItem.title = .common(.bookingsHistory)
+        doneItem.title = .common(.done)
+    }
+
+    override func configureActions() {
+        doneItem.action = #selector(customDismiss)
+    }
+
+    func startRefreshing() {
+        refreshControl.startRefreshing()
+        interactor.getBookings()
+    }
+
+    func handleBookingsSuccess() {
+        endRefreshing()
+        refreshableView.reloadData()
+        if interactor.bookings.isEmpty {
+            refreshableView.setBackground(text: .background(.noBookings))
+        }
+    }
+
+    func handleBookingsFailure(with message: String) {
+        PopUp.display(.error(description: message))
+        endRefreshing()
+        refreshableView.setBackground(text: .background(.somethingWentWrong))
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension BookingsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        interactor.bookings.count
+    }
+
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: BookingCell = tableView.dequeue(for: indexPath)
+        cell.configure(with: interactor.bookings[indexPath.item])
+        return cell
+    }
+}
