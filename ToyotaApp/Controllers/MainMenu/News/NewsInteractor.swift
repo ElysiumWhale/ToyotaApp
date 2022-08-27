7f8011448dc1e275a63b2ef0f23bacf2c57060bc
Alexey Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2022-08-27 08:11:15 +0400

    Add news interactor

---
+++ ToyotaApp/Controllers/MainMenu/News/NewsInteractor.swift
@@ -0,0 +1,58 @@
+import Foundation
+
+final class NewsInteractor {
+    private let newsService: NewsService
+
+    private var url: ShowroomsUrl {
+        ShowroomsUrl(rawValue: selectedShowroom?.id) ?? .samaraAurora
+    }
+
+    let showrooms: [Showroom] = [.aurora, .north, .south]
+
+    private(set) var selectedShowroom: Showroom?
+    private(set) var news: [News] = []
+
+    var selectedShowroomIndex: Int? {
+        showrooms.firstIndex(where: { $0.id == selectedShowroom?.id})
+    }
+
+    var onSuccessNewsLoad: Closure?
+    var onFailureNewsLoad: Closure?
+
+    init(newsService: NewsService = HtmlParser()) {
+        self.newsService = newsService
+
+        selectedShowroom = DefaultsManager.retrieve(for: .selectedShowroom) ?? .aurora
+    }
+
+    func loadNews() {
+        newsService.getNews(for: url) { [weak self] response in
+            self?.handleNewsResponse(response)
+        }
+    }
+
+    func selectShowroomIfNeeded(at index: Int) -> Bool {
+        guard showrooms.indices.contains(index) else {
+            return false
+        }
+
+        let showroom = showrooms[index]
+        if showroom.id != selectedShowroom?.id {
+            selectedShowroom = showroom
+            return true
+        }
+
+        return false
+    }
+
+    private func handleNewsResponse(_ response: Result<[News], Error>) {
+        switch response {
+        case .failure:
+            news = []
+            onFailureNewsLoad?()
+        case .success(let loadedNews):
+            news = loadedNews
+            onSuccessNewsLoad?()
+        }
+    }
+}
