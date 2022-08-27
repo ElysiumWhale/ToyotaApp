495e08a66181bc122ade93415d35022ef378b199
Alexey Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2022-08-27 08:08:33 +0400

    Fix indentations and rename string

--- ToyotaApp/Network/Logic/HtmlParser.swift
+++ ToyotaApp/Network/Logic/HtmlParser.swift
@@ -80,7 +80,13 @@ private extension String {
 }
 
 extension Showroom {
-    static let aurora = Showroom(id: "7", showroomName: "Тойота Самара Аврора", cityName: "Самара")
-    static let south = Showroom(id: "1", showroomName: "Тойота Самара Юг", cityName: "Самара")
-    static let north = Showroom(id: "2", showroomName: "Тойота Самара Север", cityName: "Самара")
+    static let aurora = Showroom(id: "7",
+                                 showroomName: "Тойота Самара Аврора",
+                                 cityName: "Самара")
+    static let south = Showroom(id: "1",
+                                showroomName: "Тойота Самара Юг",
+                                cityName: "Самара")
+    static let north = Showroom(id: "2",
+                                showroomName: "Тойота Самара Север",
+                                cityName: "Самара")
 }
