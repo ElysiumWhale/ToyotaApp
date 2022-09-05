09dbb2ffef565d51477ed5b7909b104d46c6b046
Alexey Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2022-09-05 23:33:21 +0400

    Refactor UIImage constants creating

--- ToyotaApp/Constants/UIImage+Constants.swift
+++ ToyotaApp/Constants/UIImage+Constants.swift
@@ -1,92 +1,82 @@
 import UIKit
 
 extension UIImage {
+    private static func image(name: String,
+                       scale: UIImage.SymbolScale) -> UIImage {
+        UIImage(systemName: name,
+                withConfiguration: SymbolConfiguration(scale: scale))!
+    }
+
     static var toyotaLogo: UIImage {
         #imageLiteral(resourceName: "ToyotaLogo")
     }
 
     static var newspaper: UIImage {
-        UIImage(systemName: "newspaper",
-                withConfiguration: SymbolConfiguration(scale: .medium))!
+        image(name: "newspaper", scale: .medium)
     }
 
     static var fillNewspaper: UIImage {
-        UIImage(systemName: "newspaper.fill",
-                withConfiguration: SymbolConfiguration(scale: .medium))!
+        image(name: "newspaper.fill", scale: .medium)
     }
 
     static var bookmark: UIImage {
-        UIImage(systemName: "bookmark",
-                withConfiguration: SymbolConfiguration(scale: .medium))!
+        image(name: "bookmark", scale: .medium)
     }
 
     static var fillBookmark: UIImage {
-        UIImage(systemName: "bookmark.fill",
-                withConfiguration: SymbolConfiguration(scale: .medium))!
+        image(name: "bookmark.fill", scale: .medium)
     }
 
     static var person: UIImage {
-        UIImage(systemName: "person",
-                withConfiguration: SymbolConfiguration(scale: .medium))!
+        image(name: "person", scale: .medium)
     }
 
     static var fillPerson: UIImage {
-        UIImage(systemName: "person.fill",
-                withConfiguration: SymbolConfiguration(scale: .medium))!
+        image(name: "person.fill", scale: .medium)
     }
 
     static var chat: UIImage {
-        UIImage(systemName: "text.bubble.fill",
-                withConfiguration: SymbolConfiguration(scale: .medium))!
+        image(name: "text.bubble.fill", scale: .medium)
     }
 
     static var send: UIImage {
-        UIImage(systemName: "arrow.up.square.fill",
-                withConfiguration: SymbolConfiguration(scale: .medium))!
+        image(name: "arrow.up.square.fill", scale: .medium)
     }
 
     static var timeDone: UIImage {
-        UIImage(systemName: "clock.badge.checkmark.fill",
-                withConfiguration: UIImage.SymbolConfiguration(scale: .small))!
+        image(name: "clock.badge.checkmark.fill", scale: .small)
     }
 
     static var timeAlert: UIImage {
-        UIImage(systemName: "clock.badge.exclamationmark.fill",
-                withConfiguration: UIImage.SymbolConfiguration(scale: .small))!
+        image(name: "clock.badge.exclamationmark.fill", scale: .small)
     }
 
     static var statusCirle: UIImage {
-        UIImage(systemName: "circle.fill",
-                withConfiguration: UIImage.SymbolConfiguration(scale: .small))!
+        image(name: "circle.fill", scale: .small)
     }
 
     static var personFill: UIImage {
-        UIImage(systemName: "person.fill",
-                withConfiguration: SymbolConfiguration(scale: .medium))!
+        image(name: "person.fill", scale: .medium)
     }
 
     static var trashFill: UIImage {
-        UIImage(systemName: "trash.fill",
-                withConfiguration: SymbolConfiguration(scale: .large))!
+        image(name: "trash.fill", scale: .large)
     }
 
     static var settings: UIImage {
-        UIImage(systemName: "gearshape",
-                withConfiguration: SymbolConfiguration(scale: .large))!
+        image(name: "gearshape", scale: .large)
     }
 
     static var logout: UIImage {
-        UIImage(systemName: "rectangle.righthalf.inset.fill.arrow.right",
-                withConfiguration: SymbolConfiguration(scale: .large))!
+        image(name: "rectangle.righthalf.inset.fill.arrow.right",
+              scale: .large)
     }
 
     static var bookings: UIImage {
-        UIImage(systemName: "archivebox",
-                withConfiguration: SymbolConfiguration(scale: .medium))!
+        image(name: "archivebox", scale: .medium)
     }
 
     static var car: UIImage {
-        UIImage(systemName: "car",
-                withConfiguration: SymbolConfiguration(scale: .medium))!
+        image(name: "car", scale: .medium)
     }
 }
