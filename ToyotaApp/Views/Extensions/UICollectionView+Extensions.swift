a2b6556745aa3048d92fbbcecc3244fec48bee78
Alexey Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2022-12-18 04:56:48 +0400

    Remove redundant code

--- ToyotaApp/Views/Extensions/UICollectionView+Extensions.swift
+++ ToyotaApp/Views/Extensions/UICollectionView+Extensions.swift
@@ -3,54 +3,6 @@ import UIKit
 typealias CVCellRegistration<T1: UICollectionViewCell, T2> = UICollectionView.CellRegistration<T1, T2>
 typealias CVDataSource<T1: Hashable, T2: Hashable> = UICollectionViewDiffableDataSource<T1, T2>
 
-// MARK: - Helpers
-extension UICollectionView {
-    convenience init(layout: UICollectionViewLayout) {
-        self.init(frame: .zero, collectionViewLayout: layout)
-    }
-
-    func setBackground(text: String?) {
-        backgroundView = createBackground(labelText: text)
-    }
-
-    func registerCell(_ cellClass: UICollectionViewCell.Type) {
-        register(
-            cellClass,
-            forCellWithReuseIdentifier: String(describing: cellClass)
-        )
-    }
-
-    func dequeue<TCell: UICollectionViewCell>(for indexPath: IndexPath) -> TCell {
-        let id = String(describing: TCell.self)
-        guard let cell = dequeueReusableCell(withReuseIdentifier: id,
-                                             for: indexPath) as? TCell else {
-            assertionFailure("Can't dequeue cell.")
-            return TCell()
-        }
-
-        return cell
-    }
-
-    func change<TCell: UICollectionViewCell>(
-        _ cellType: TCell.Type,
-        at indexPath: IndexPath,
-        _ changeAction: @escaping ParameterClosure<TCell>
-    ) {
-
-        guard let cell = cellForItem(at: indexPath) as? TCell else {
-            return
-        }
-
-        UIViewPropertyAnimator.runningPropertyAnimator(
-            withDuration: 0.2,
-            delay: 0,
-            options: [.curveEaseOut]
-        ) {
-            changeAction(cell)
-        }
-    }
-}
-
 // MARK: - Collection View Layouts
 extension UICollectionViewLayout {
     static var servicesLayout: UICollectionViewCompositionalLayout {
--- ToyotaApp/Views/Extensions/UIExtensions.swift
+++ ToyotaApp/Views/Extensions/UIExtensions.swift
@@ -1,130 +1,57 @@
 import UIKit
+import DesignKit
 
-// MARK: - UITableView
-extension UITableView {
-    func setBackground(text: String?) {
-        backgroundView = createBackground(labelText: text)
-    }
-
-    func registerCell(_ cellClass: UITableViewCell.Type) {
-        register(cellClass, forCellReuseIdentifier: String(describing: cellClass))
-    }
-
-    func dequeue<TCell: UITableViewCell>(for indexPath: IndexPath) -> TCell {
-        guard let cell = dequeueReusableCell(withIdentifier: String(describing: TCell.self),
-                                             for: indexPath) as? TCell else {
-            assertionFailure("Can't dequeue cell.")
-            return TCell()
-        }
-
-        return cell
-    }
-}
-
-// MARK: - UITextField
-extension UITextField {
-    enum FieldState {
-        case error
-        case normal
-    }
-
-    func toggle(state: FieldState) {
-        let hasError = state == .error
-        UIViewPropertyAnimator.runningPropertyAnimator(
-            withDuration: 0.2,
-            delay: 0,
-            options: [.curveEaseOut],
-            animations: {
-                self.layer.borderColor = hasError ? UIColor.systemRed.cgColor : UIColor.clear.cgColor
-                self.layer.borderWidth = hasError ? 1 : 0
-            }
+extension UIView {
+    static func titleViewFor(
+        city: String? = nil,
+        action: @escaping () -> Void
+    ) -> UIView {
+        let button = UIButton.titleButton(
+            with: city ?? .common(.chooseCity),
+            action: action
         )
+        let rightButton = UIButton(frame: .init(
+            x: 0, y: 0, width: 20, height: 20)
+        )
+        rightButton.setImage(UIImage(systemName: "chevron.right"),
+                             for: .normal)
+        rightButton.tintColor = .appTint(.secondarySignatureRed)
+        rightButton.addAction(action)
+
+        let container = UIView(frame: .init(
+            x: 0, y: 0, width: 100, height: 30)
+        )
+        container.addSubviews(button, rightButton)
+
+        button.edgesToSuperview(excluding: .trailing)
+        rightButton.trailingToSuperview()
+        button.trailingToLeading(of: rightButton)
+        button.centerY(to: rightButton, offset: -3)
+
+        return container
     }
 
-    func setRightView(from view: UIView, width: Double = 30, height: Double) {
-        NSLayoutConstraint.deactivate(rightView?.constraints ?? [])
-        rightView = nil
-        let rect = CGRect(x: 0, y: 0, width: width, height: height)
-        view.frame = rect
-        let resultView = UIView(frame: rect)
-        resultView.addSubview(view)
-        NSLayoutConstraint.activate([
-            view.centerYAnchor.constraint(equalTo: resultView.centerYAnchor),
-            view.trailingAnchor.constraint(equalTo: resultView.trailingAnchor, constant: -10)
-        ])
-        rightView = resultView
-    }
-}
-
-// MARK: - UICollectionViewCell
-extension UICollectionViewCell {
-    func configureShadow(with cornerRadius: CGFloat, shadowRadius: CGFloat = 3) {
-        layer.shadowColor = UIColor.black.cgColor.copy(alpha: 0.5)
-        layer.shadowOffset = CGSize(width: 0.0, height: 3.0)
-        layer.shadowRadius = shadowRadius
-        layer.shadowOpacity = 0.7
-        layer.masksToBounds = false
-        layer.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius).cgPath
-    }
-}
-
-// MARK: - UIControl
-extension UIControl {
-    func addAction(for controlEvents: UIControl.Event = .touchUpInside,
-                   _ closure: @escaping Closure) {
-        addAction(UIAction { _ in closure() }, for: controlEvents)
-    }
-}
-
-// MARK: - UIRefreshControl
-extension UIRefreshControl {
-    func startRefreshing(title: String = .common(.loading)) {
-        attributedTitle = NSAttributedString(string: title)
-        beginRefreshing()
-    }
-
-    func stopRefreshing(title: String = .common(.pullToRefresh)) {
-        endRefreshing()
-        attributedTitle = NSAttributedString(string: title)
-    }
-
-    func refreshManually() {
-        if let scrollView = superview as? UIScrollView {
-            let y = scrollView.contentOffset.y - frame.height
-            scrollView.setContentOffset(CGPoint(x: 0, y: y), animated: true)
-        }
-
-        beginRefreshing()
-        sendActions(for: .valueChanged)
-    }
-}
-
-// MARK: - UIPickerView
-extension UIPickerView {
-    func configure(delegate: UIPickerViewDelegate & UIPickerViewDataSource,
-                   with action: Selector,
-                   for textField: UITextField) {
-        self.dataSource = delegate
-        self.delegate = delegate
-        textField.inputAccessoryView = .buildToolbar(with: action, target: delegate)
-        textField.inputView = self
-    }
-
-    var selectedRow: Int {
-        selectedRow(inComponent: 0)
-    }
-}
-
-// MARK: - UIDatePicker
-extension UIDatePicker {
-    func configure(with action: Selector,
-                   for textField: UITextField) {
-        preferredDatePickerStyle = .wheels
-        locale = Locale(identifier: "ru")
-        datePickerMode = .date
-        maximumDate = Date()
-        textField.inputAccessoryView = .buildToolbar(with: action)
-        textField.inputView = self
+    // MARK: - Toolbar for controls
+    static func buildToolbar(
+        with action: Selector,
+        target: Any? = nil
+    ) -> UIToolbar {
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        let flexible = UIBarButtonItem(
+            barButtonSystemItem: .flexibleSpace,
+            target: target,
+            action: nil
+        )
+        let doneButton = UIBarButtonItem(
+            title: .common(.choose),
+            style: .done,
+            target: target,
+            action: action
+        )
+        doneButton.tintColor = .appTint(.secondarySignatureRed)
+        toolBar.setItems([flexible, doneButton], animated: true)
+        return toolBar
     }
 }
 
@@ -174,24 +101,11 @@ extension UIColor {
     }
 }
 
-// MARK: - City picking cell coniguration
-extension UIContentConfiguration where Self == UIListContentConfiguration {
-    static func cellConfiguration(with text: String, isSelected: Bool) -> Self {
-        var result = UIListContentConfiguration.cell()
-        result.text = text
-        result.textProperties.color = isSelected
-            ? .white
-            : .appTint(.signatureGray)
-
-        return result
-    }
-}
-
 // MARK: - UIButton
 extension UIButton {
     static func titleButton(
         with text: String,
-        action: @escaping Closure
+        action: @escaping () -> Void
     ) -> UIButton {
         let button =  UIButton(type: .custom)
         button.frame = CGRect(x: 0, y: 0, width: 100, height: 40)
@@ -206,7 +120,7 @@ extension UIButton {
 
     static func imageButton(
         imageName: String = "chevron.down",
-        action: Closure? = nil
+        action: (() -> Void)? = nil
     ) -> UIButton {
         let button = UIButton()
         let image = UIImage(systemName: imageName)
@@ -221,17 +135,15 @@ extension UIButton {
     }
 }
 
-// MARK: - UIStackView
-extension UIStackView {
-    func addArrangedSubviews(_ views: UIView...) {
-        for view in views {
-            addArrangedSubview(view)
-        }
-    }
+// MARK: - City picking cell configuration
+extension UIContentConfiguration where Self == UIListContentConfiguration {
+    static func cellConfiguration(with text: String, isSelected: Bool) -> Self {
+        var result = UIListContentConfiguration.cell()
+        result.text = text
+        result.textProperties.color = isSelected
+            ? .white
+            : .appTint(.signatureGray)
 
-    func addArrangedSubviews(_ views: [UIView]) {
-        for view in views {
-            addArrangedSubview(view)
-        }
+        return result
     }
 }
