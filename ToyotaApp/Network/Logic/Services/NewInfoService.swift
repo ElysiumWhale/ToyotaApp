9e9fb5279256d243c0d0b2beb3ded94f14eae079
Alexey Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2022-12-26 01:45:57 +0400

    Add IRegistrationService and implementation

--- ToyotaApp/Network/Logic/Services/NewInfoService.swift
+++ ToyotaApp/Network/Logic/Services/NewInfoService.swift
@@ -9,6 +9,13 @@ protocol IAuthService {
     ) async -> Result<CheckUserOrSmsCodeResponse, ErrorResponse>
 }
 
+protocol IRegistrationService {
+    func registerPhone(_ body: RegisterPhoneBody) async -> DefaultResponse
+    func changePhone(_ body: ChangePhoneBody) async -> DefaultResponse
+    func deleteTemporaryPhone(_ body: DeletePhoneBody) async -> DefaultResponse
+    func checkCode(_ body: CheckSmsCodeBody) async -> NewResponse<CheckUserOrSmsCodeResponse>
+}
+
 actor NewInfoService {
     private let networkService: NewNetworkService
 
@@ -24,6 +31,37 @@ extension NewInfoService: IAuthService {
     ) async -> Result<CheckUserOrSmsCodeResponse, ErrorResponse> {
         await networkService.makeRequest(Request(
             page: .start(.checkUser), body: body
+        ))
+    }
+}
+
+// MARK: - IRegistrationService
+extension NewInfoService: IRegistrationService {
+    func registerPhone(_ body: RegisterPhoneBody) async -> DefaultResponse {
+        await networkService.makeRequest(Request(
+            page: .registration(.registerPhone), body: body
+        ))
+    }
+
+    func changePhone(_ body: ChangePhoneBody) async -> DefaultResponse {
+        await networkService.makeRequest(Request(
+            page: .setting(.changePhone), body: body
+        ))
+    }
+
+    func deleteTemporaryPhone(
+        _ body: DeletePhoneBody
+    ) async -> DefaultResponse {
+        await networkService.makeRequest(Request(
+            page: .registration(.deleteTemp), body: body
+        ))
+    }
+
+    func checkCode(
+        _ body: CheckSmsCodeBody
+    ) async -> NewResponse<CheckUserOrSmsCodeResponse> {
+        await networkService.makeRequest(Request(
+            page: .registration(.checkCode), body: body
         ))
     }
 }
