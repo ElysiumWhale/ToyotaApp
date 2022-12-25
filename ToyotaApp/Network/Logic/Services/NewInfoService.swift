a3eabac24cbe53e960096577401fd4cbc6a72884
Alexey Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2022-12-26 01:13:28 +0400

    Rework SceneDelegate with new network service

--- ToyotaApp/Network/Logic/Services/NewInfoService.swift
+++ ToyotaApp/Network/Logic/Services/NewInfoService.swift
@@ -3,10 +3,27 @@ import Foundation
 typealias DefaultResponse = Result<SimpleResponse, ErrorResponse>
 typealias NewResponse<TResponse> = Result<TResponse, ErrorResponse>
 
+protocol IAuthService {
+    func checkUser(
+        _ body: CheckUserBody
+    ) async -> Result<CheckUserOrSmsCodeResponse, ErrorResponse>
+}
+
 actor NewInfoService {
     private let networkService: NewNetworkService
 
     init(networkService: NewNetworkService = .init()) {
         self.networkService = networkService
+    }
+}
+
+// MARK: - IAuthService
+extension NewInfoService: IAuthService {
+    func checkUser(
+        _ body: CheckUserBody
+    ) async -> Result<CheckUserOrSmsCodeResponse, ErrorResponse> {
+        await networkService.makeRequest(Request(
+            page: .start(.checkUser), body: body
+        ))
     }
 }
