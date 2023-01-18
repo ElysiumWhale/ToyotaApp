3f741a7952c96e1892a801f763a2f6565a864174
Alexey Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2023-01-19 03:22:30 +0400

    Rework personal info module tests

--- ToyotaAppTests/Modules/PersonalInfoModuleTests.swift
+++ ToyotaAppTests/Modules/PersonalInfoModuleTests.swift
@@ -2,75 +2,26 @@ import XCTest
 @testable import ToyotaApp
 
 final class PersonalInfoVCTest: XCTestCase {
-    var controller: PersonalInfoView?
-
-    override func setUpWithError() throws {
-        controller = RegisterFlow.personalModule() as? PersonalInfoView
+    func testEmptyModuleState() throws {
+        let controller = RegisterFlow.personalModule() as? PersonalInfoView
+        XCTAssertEqual(controller?.interactor.state, .empty)
     }
 
-    override func tearDownWithError() throws {
-        controller = nil
-    }
-
-    func testModuleParts() throws {
-        XCTAssertNotNil(controller)
-        XCTAssertNotNil(controller?.interactor)
-    }
-
-    func testModuleState() throws {
-        try testModuleParts()
-
-        var result = false
-        if case .empty = controller!.interactor.state {
-            result = true
-        }
-
-        XCTAssert(result)
+    func testConfiguredModuleState() throws {
+        let controller = RegisterFlow.personalModule(.mock) as? PersonalInfoView
+        XCTAssertEqual(controller?.interactor.state, .configured(with: .mock))
     }
 }
 
-class ConfiguredPersonalInfoVCTest: XCTestCase {
-    let firstName = "Valera"
-    let lastName = "Ivanov"
-    let secondName = "Ivanovich"
-    let email = "aboba@aboba.com"
-    let birthday = "2021-05-05"
-
-    var configuredController: PersonalInfoView?
-
-    override func setUpWithError() throws {
-        let profile = Profile(phone: .empty,
-                              firstName: firstName,
-                              lastName: lastName,
-                              secondName: secondName,
-                              email: email,
-                              birthday: birthday)
-
-        configuredController = RegisterFlow.personalModule(profile) as? PersonalInfoView
-    }
-
-    override func tearDownWithError() throws {
-        configuredController = nil
-    }
-
-    func testModuleParts() throws {
-        XCTAssertNotNil(configuredController)
-        XCTAssertNotNil(configuredController?.interactor)
-    }
-
-    func testModuleState() throws {
-        try testModuleParts()
-
-        var configuredResult = false
-        if case .configured(let profile) = configuredController!.interactor.state {
-            XCTAssertEqual(profile.firstName, firstName)
-            XCTAssertEqual(profile.secondName, secondName)
-            XCTAssertEqual(profile.lastName, lastName)
-            XCTAssertEqual(profile.email, email)
-            XCTAssertEqual(profile.birthday, birthday)
-            configuredResult = true
-        }
-
-        XCTAssert(configuredResult)
+private extension Profile {
+    static var mock: Profile {
+        .init(
+            phone: "123",
+            firstName: "Valera",
+            lastName: "Ivanov",
+            secondName: "Ivanovich",
+            email: "aboba@aboba.com",
+            birthday: "2021-05-05"
+        )
     }
 }
