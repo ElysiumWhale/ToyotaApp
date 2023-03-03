1e7c9650839a14b5bc771c076f1c0f2666809882
Alexey Gurin <38838291+ElysiumWhale@users.noreply.github.com>
2023-03-04 01:19:56 +0400

    Fix SmsCodeFeature tests

--- ToyotaAppTests/Modules/SmsCodeFeatureTests.swift
+++ ToyotaAppTests/Modules/SmsCodeFeatureTests.swift
@@ -39,6 +39,9 @@ final class SmsCodeFeatureTests: XCTestCase {
             $0.isLoading = false
             $0.popupMessage = message
         }
+        await store.receive(.popupDidShow) {
+            $0.popupMessage = nil
+        }
     }
 
     func testCheckValidCodeSuccess() async {
